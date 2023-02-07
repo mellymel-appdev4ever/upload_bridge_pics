350b05ab760417b57608570369d970b0236369cf
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-02-06 17:26:30 -0800

    Update index.py

--- index.py
+++ index.py
@@ -32,7 +32,7 @@ country_codes_df =  pd.DataFrame(country_codes_df)
 #st.write(f"You selected option {option} called {format_func(option)}")
 
 display = (country_codes_df.columns[1])
-options = (country_codes_df.columns[2])
+options = (country_codes_df.columns[0])
 value = st.selectbox("Pick a Country", options, format_func=lambda x: display[x])
 
 st.write(value)
