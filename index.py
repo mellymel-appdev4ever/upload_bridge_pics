224b8fcd862e984f450bd32f0a499e82a76d5d5c
MellyMel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-02-06 16:36:48 -0800

    Update index.py

--- index.py
+++ index.py
@@ -31,7 +31,7 @@ session = create_session()
 country_codes_df = session.sql("select iso_country_name, alpha_code_2digit from intl_db.countries.int_stds_org_3661 order by iso_country_name;").collect()
 country_codes_df =  pd.DataFrame(country_codes_df)
 #country_codes_df = country_codes_df["alpha_code_2digit"]
-st.write(country_codes_df[alpha_code_2digit]) 
+st.write(country_codes_df[]) 
  
 col1, col2 = st.columns(2)
 
