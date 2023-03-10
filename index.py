818fc1f0cc16efdf84b7c2b4755342566ded6ac1
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-09 16:45:25 -0800

    Update index.py

--- index.py
+++ index.py
@@ -83,7 +83,7 @@ if uploaded_file is not None:
         rek_response = rek.detect_labels(
               Image={'S3Object':{'Bucket':bucket,'Name':file_to_put}},
               MaxLabels=10,
-              Settings={"GeneralLabels": {"LabelInclusionFilters":["bridge", "dog", "bagel"]}}
+              Settings={"GeneralLabels": {"LabelInclusionFilters":["Bridge", "Dog", "Bagel"]}}
               )                                    
     
         st.write('The image you loaded has been examined for the presence of Bridges, Beagles and Bagels and found the following probabilities:')
