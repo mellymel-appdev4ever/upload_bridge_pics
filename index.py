de5f2c21a6ae252106cd0c89c9a6bdae131ec8e6
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-09 16:35:30 -0800

    Update index.py

--- index.py
+++ index.py
@@ -92,7 +92,7 @@ if uploaded_file is not None:
         all_confidences = [label['Confidence'] for label in rek_response['Labels']]
         all_labels = [label['Name'] for label in rek_response['Labels']]
         for i in range(0, len(all_labels)):
-                all_labels[i]=all_labels[i]+": "+str(all_confidences[i])+"%"         
+                all_labels[i]=all_labels[i]+": "+str(int(all_confidences[i]))+"%"         
         
         st.write(str(all_labels))
         st.stop()
