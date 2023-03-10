c63fa674796c638021545ee4f837a1398af3c26c
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-09 16:14:44 -0800

    Update index.py

--- index.py
+++ index.py
@@ -87,8 +87,10 @@ if uploaded_file is not None:
               )                                    
     
         st.write(rek_response)
-        all_confidences = [label['Confidence'] for label in response['Labels']]
-        all_labels = [label['Name'] for label in response['Labels']]
+        rr_df = pd.DataFrame(rek_response)
+        st.write(rr_df)
+        #all_confidences = [label['Confidence'] for label in response['Labels']]
+        #all_labels = [label['Name'] for label in response['Labels']]
         #for i in range(0, len(all_labels)):
                 #all_labels[i]=all_labels[i]+": "+str(all_confidences[i])+"%"         
         
