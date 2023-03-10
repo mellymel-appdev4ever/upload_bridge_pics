5ffe7c82a83861d9277de8b2bd1baf0635bc6a08
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-09 16:06:50 -0800

    Update index.py

--- index.py
+++ index.py
@@ -87,10 +87,10 @@ if uploaded_file is not None:
               )                                    
     
         st.write(rek_response)
-        #all_confidences = [label['Confidence'] for label in response['Labels']]
-        #all_labels = [label['Name'] for label in response['Labels']]
-        #for i in range(0, len(all_labels)):
-        #        all_labels[i]=all_labels[i]+": "+str(all_confidences[i])+"%"         
+        all_confidences = [label['Confidence'] for label in response['Labels']]
+        all_labels = [label['Name'] for label in response['Labels']]
+        for i in range(0, len(all_labels)):
+                all_labels[i]=all_labels[i]+": "+str(all_confidences[i])+"%"         
         
         st.write('Thanks for uploading this wonderful image!')
         st.stop()
