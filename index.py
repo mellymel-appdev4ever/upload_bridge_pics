6d8abf4023bd2ed9abad2cc95ac0204bab124926
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-09 17:18:32 -0800

    Update index.py

--- index.py
+++ index.py
@@ -59,7 +59,8 @@ with col1:
 with col2:
    bridge_name = st.text_input('Bridge Name', 'Øresund')
    st.write('The bridge name you entered is:', bridge_name)
-   
+
+with container():
 st.write('Please choose a JPG or PNG file to add to our bridge collection.')   
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False, label_visibility='hidden')
 
