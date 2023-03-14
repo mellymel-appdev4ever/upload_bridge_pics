a4381c315da03ff40f857ee60c6e4ea66df6d922
Mel Melnyk <103217003+mellymel-appdev4ever@users.noreply.github.com>
2023-03-13 18:06:34 -0700

    Update index.py

--- index.py
+++ index.py
@@ -97,7 +97,7 @@ with st.container():
          for label in rek_response['Labels']:
              st.write("Item Identified: " + label['Name'])
              st.write("Percentage Confidence in Item Identification: " + str(label['Confidence']))
-             if label['Name'] = "Bridge": 
+             if label['Name'] is "Bridge": 
                   bridge_conf_level = label['Confidence']
              for instance in label['Instances']:
                  box = instance['BoundingBox']
